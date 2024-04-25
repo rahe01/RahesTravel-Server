9dbea666fe5b81a91d378a26dbedd49009dcc985
Rahe <130391604+rahe01@users.noreply.github.com>
2024-04-25 20:47:23 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Helloooooo World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
